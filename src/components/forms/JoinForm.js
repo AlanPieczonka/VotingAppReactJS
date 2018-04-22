e1cf61e19993b764c059031f3da52ad55e0cdc32
AlanPieczonka <alanpieczonka@gmail.com>
2018-04-22 19:32:29 +0200

    Create forms folder and JoinForm component

---
+++ src/components/forms/JoinForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class JoinForm extends Component {
+  render() {
+    return (
+      <div style={{ border: '2px solid black' }}>
+        <h2 className="weight300">This is JoinForm</h2>
+      </div>
+    );
+  }
+}
+
+export default JoinForm;
--- src/components/pages/JoinPage.js
+++ src/components/pages/JoinPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { JoinForm } from '../forms/JoinForm';
 
 export default class JoinPage extends Component {
   render() {
     return (
       <div>
         <h1 className="weight300">JoinPage</h1>
+        <JoinForm />
       </div>
     );
   }
