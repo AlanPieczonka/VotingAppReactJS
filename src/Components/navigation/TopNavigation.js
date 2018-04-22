14081fa9b16d19542556111d2186b37ca6db9546
AlanPieczonka <alanpieczonka@gmail.com>
2018-04-22 18:14:13 +0200

    Configure eslint

--- src/Components/navigation/TopNavigation.js
+++ src/Components/navigation/TopNavigation.js
@@ -8,46 +8,47 @@ import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import HomeIcon from '@material-ui/icons/Home';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
+
 const styles = {
-	root: {
-		flexGrow: 1
-	},
-	flex: {
-		flex: 1
-	},
-	menuButton: {
-		marginLeft: -12,
-		marginRight: 20
-	}
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
 };
 
 function ButtonAppBar(props) {
-	const { classes } = props;
-	return (
-		<div className={classes.root}>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
             <Link to="/" className="link">
               <HomeIcon />
             </Link>
-					</IconButton>
-					<Typography variant="title" color="inherit" className={classes.flex}>
+          </IconButton>
+          <Typography variant="title" color="inherit" className={classes.flex}>
 						Voting App
-					</Typography>
-					<Link to="/about" className="link">
-					  <Button color="inherit">About</Button>
-					</Link>
-					<Button color="inherit">Login</Button>
-					<Button color="inherit">Join</Button>
-				</Toolbar>
-			</AppBar>
-		</div>
-	);
+          </Typography>
+          <Link to="/about" className="link">
+            <Button color="inherit">About</Button>
+          </Link>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Join</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 ButtonAppBar.propTypes = {
-	classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(ButtonAppBar);
--- src/Components/routes/About.js
+++ src/Components/routes/About.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-const About = () => {
-  return (
-    <div>
-        <h1 className="weight300">About</h1>
-        <h2 className="weight300">This app is currently under development</h2>
-    </div>
-  )
-}
+const About = () => (
+  <div>
+    <h1 className="weight300">About</h1>
+    <h2 className="weight300">This app is currently under development</h2>
+  </div>
+);
 
 export default About;
