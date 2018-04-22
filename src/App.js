cd070d3866380242f7e6245c5ef9e346df2922cc
Alan Pieczonka <alanpieczonka@gmail.com>
2018-04-22 04:14:22 +0200

    Place TopNavigation and routes/About component in the App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './Components/routes/About';
+import TopNavigation from './Components/navigation/TopNavigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React. It seems to work</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TopNavigation />
+        <Route exact path="/about" component={About} />
       </div>
     );
   }
