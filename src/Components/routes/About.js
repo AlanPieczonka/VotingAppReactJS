7a05383447220fdad8533718457b96f9c251090d
Alan Pieczonka <alanpieczonka@gmail.com>
2018-04-22 04:13:38 +0200

    Create routes/About component

---
+++ src/Components/routes/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1 className="weight300">About</h1>
+        <h2 className="weight300">This app is currently under development</h2>
+    </div>
+  )
+}
+
+export default About;
