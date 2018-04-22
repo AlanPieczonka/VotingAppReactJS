e12110c7acbd4239083f508ea954c8a91013d329
AlanPieczonka <alanpieczonka@gmail.com>
2018-04-22 18:31:16 +0200

    Rewrite App.js as functional component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import About from './Components/routes/About';
 import TopNavigation from './Components/navigation/TopNavigation';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TopNavigation />
-        <Route exact path="/about" component={About} />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <TopNavigation />
+      <Route exact path="/about" component={About} />
+    </div>
+  );
 }
+
 
 export default App;
