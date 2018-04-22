4f9afdc18d8fe5dffbdf00025f268373989f000c
AlanPieczonka <alanpieczonka@gmail.com>
2018-04-22 19:08:15 +0200

    Create /pages folder and move About route there

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
-import About from './Components/routes/About';
+import AboutPage from './Components/pages/AboutPage';
 import TopNavigation from './Components/navigation/TopNavigation';
 
 const App = () => {
   return (
     <div className="App">
       <TopNavigation />
-      <Route exact path="/about" component={About} />
+      <Route exact path="/about" component={AboutPage} />
     </div>
   );
 }
--- src/Components/pages/AboutPage.js
+++ src/Components/pages/AboutPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const About = () => (
+const AboutPage = () => (
   <div>
     <h1 className="weight300">About</h1>
     <h2 className="weight300">This app is currently under development</h2>
   </div>
 );
 
-export default About;
+export default AboutPage;
