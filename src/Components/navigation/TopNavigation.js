65859c7924e9e8f78dd27d49b1c38c68a63720d1
Alan Pieczonka <alanpieczonka@gmail.com>
2018-04-22 04:13:09 +0200

    Create TopNavigation component

---
+++ src/Components/navigation/TopNavigation.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+const styles = {
+	root: {
+		flexGrow: 1
+	},
+	flex: {
+		flex: 1
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20
+	}
+};
+
+function ButtonAppBar(props) {
+	const { classes } = props;
+	return (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <Link to="/" className="link">
+              <HomeIcon />
+            </Link>
+					</IconButton>
+					<Typography variant="title" color="inherit" className={classes.flex}>
+						Voting App
+					</Typography>
+					<Link to="/about" className="link">
+					  <Button color="inherit">About</Button>
+					</Link>
+					<Button color="inherit">Login</Button>
+					<Button color="inherit">Join</Button>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+}
+
+ButtonAppBar.propTypes = {
+	classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ButtonAppBar);
