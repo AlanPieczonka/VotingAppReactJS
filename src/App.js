88d08bf70ae186e08cbc2ea9c2598470628788b0
Alan Pieczonka <alanpieczonka@gmail.com>
2018-04-24 20:18:35 +0200

    Install redux and react-redux

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import TopNavigation from './components/navigation/TopNavigation';
 import AboutPage from './components/pages/AboutPage';
 import JoinPage from './components/pages/JoinPage';
 import LoginPage from './components/pages/LoginPage';
-const App = () => {
-  return (
-    <div className="App">
-      <TopNavigation />
-      <Switch>
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/join" component={JoinPage} />
-        <Route exact path="/login" component={LoginPage} />
-      </Switch>
-    </div>
-  );
-}
+
+const App = () => (
+  <div className="App">
+    <TopNavigation />
+    <Switch>
+      <Route exact path="/about" component={AboutPage} />
+      <Route exact path="/join" component={JoinPage} />
+      <Route exact path="/login" component={LoginPage} />
+    </Switch>
+  </div>
+);
 
 
 export default App;
