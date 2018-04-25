71cd71eec3be0ffa1ee3bd1798aae7170405303b
Alan Pieczonka <alanpieczonka@gmail.com>
2018-04-25 09:42:40 +0200

    Move submit() action to JoinPage component

--- src/components/forms/JoinForm.js
+++ src/components/forms/JoinForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
 
@@ -9,44 +10,38 @@ import TextField from 'material-ui/TextField';
 // <Join /> button
 // Already a member? link
 
-const submit = (e) => {
-  e.preventDefault();
-  console.log('Some fake submit action');
-};
-
-
 const JoinForm = (props = []) => (
   <div>
     <div style={{ border: '2px solid black', padding: '5%' }}>
       <h2 className="weight300">This is JoinForm</h2>
       <div>
-        <form onSubmit={submit}>
-        <div>
-          <TextField
-            id="email-input"
-            label="Email"
-            margin="normal"
-            fullWidth
-          />
-        </div>
-        <div>
-        <TextField
-          id="password-input"
-          label="Password"
-          type="password"
-          margin="normal"
-          fullWidth
-        />
-        </div>
-        <div>
-        <TextField
-          id="password-confirmation-input"
-          label="Password confirmation"
-          type="password"
-          margin="normal"
-          fullWidth
-        />
-        </div>
+        <form onSubmit={props.submit}>
+          <div>
+            <TextField
+              id="email-input"
+              label="Email"
+              margin="normal"
+              fullWidth
+            />
+          </div>
+          <div>
+            <TextField
+              id="password-input"
+              label="Password"
+              type="password"
+              margin="normal"
+              fullWidth
+            />
+          </div>
+          <div>
+            <TextField
+              id="password-confirmation-input"
+              label="Password confirmation"
+              type="password"
+              margin="normal"
+              fullWidth
+            />
+          </div>
           <Button type="submit" variant="raised" color="primary">
               Join
           </Button>
@@ -56,5 +51,9 @@ const JoinForm = (props = []) => (
     </div>
   </div>
 );
+
+JoinForm.propTypes = {
+  submit: PropTypes.func.isRequired,
+};
 
 export default JoinForm;
--- src/components/pages/JoinPage.js
+++ src/components/pages/JoinPage.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import  JoinForm  from '../forms/JoinForm';
+import JoinForm from '../forms/JoinForm';
 
 export default class JoinPage extends Component {
+  submit(e) {
+    e.preventDefault();
+    console.log('Submit action');
+  }
   render() {
     return (
       <div>
         <h1 className="weight300">JoinPage</h1>
-        <JoinForm />
+        <JoinForm submit={this.submit} />
       </div>
     );
   }
