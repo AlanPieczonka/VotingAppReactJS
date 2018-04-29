8bd608a08d8b0dbd7605117af67fa500fc15d11c
Alan Pieczonka <alanpieczonka@gmail.com>
2018-04-29 22:44:39 +0200

    Create src/actions/join.js file with signup action

---
+++ src/actions/join.js
@@ -0,0 +1,9 @@
+import api from '../api';
+import { userLoggedIn } from './auth';
+
+export const signup = data => dispatch =>
+  api.user.signup(data).then((user) => {
+    localStorage.votingappJWT = user.token;
+    dispatch(userLoggedIn(user));
+  });
+
