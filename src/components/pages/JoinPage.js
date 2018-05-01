322e718b724b4eeca6611c91cd76cde1e93d68d8
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-01 16:12:04 +0200

    Display MessageSnackbar if there is any error in JoinPage component, ex. when User's email is already taken

--- src/components/pages/JoinPage.js
+++ src/components/pages/JoinPage.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import JoinForm from '../forms/JoinForm';
 import { signup } from '../../actions/join';
+import MessageSnackbar from '../messages/MessageSnackbar';
 
-const JoinPage = (props) => {
-  const handleSignIn = values => props.signup(values).then(props.history.push('/'));
-  return (
-    <JoinForm
-      onSubmit={handleSignIn}
-    />
-  );
-};
+class JoinPage extends Component {
+  state = {
+    error: false,
+  }
+  handleSignIn = (values) => {
+    this.props.signup(values)
+      .then(() => this.props.history.push('/'))
+      .catch((err) => {
+        const error = err.response.data.errors.email || 'There has been an error';
+        this.setState({ error });
+      });
+  }
+  render() {
+    const { error } = this.state;
+    return (
+      <Fragment>
+        {error && <MessageSnackbar message={error} /> }
+        <JoinForm
+          onSubmit={this.handleSignIn}
+        />
+      </Fragment>
+    );
+  }
+}
 
 JoinPage.propTypes = {
   signup: PropTypes.func.isRequired,
