4e8990a283e3ca9ddd22bfbd2b45a5251a15cad0
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-01 16:20:23 +0200

    Automatically login user from localStorage if present

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import decode from 'jwt-decode';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
+import { userLoggedIn } from './actions/auth';
+
+if (localStorage.votingappJWT) {
+  const payload = decode(localStorage.votingappJWT);
+  const user = {
+    email: payload.email,
+    token: localStorage.votingappJWT,
+  };
+  store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(
   <BrowserRouter>
