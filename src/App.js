1125398c6ad2509599a724c54d0ba010cc196cd1
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-04 10:59:47 +0200

    Display Snackbar in App.js and poorly prevent logged-in users from accessing /join and /login routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import isEmpty from 'lodash/isEmpty';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import TopNavigation from './components/navigation/TopNavigation';
 import AboutPage from './components/pages/AboutPage';
 import JoinPage from './components/pages/JoinPage';
 import LoginPage from './components/pages/LoginPage';
+import HomePage from './components/pages/HomePage';
+import MessageSnackbar from './components/messages/MessageSnackbar';
+import NotFoundPage from './components/pages/NotFoundPage';
 
-const App = () => (
+const App = props => (
   <div className="App">
     <TopNavigation />
+    {props.snackbarMessage.length > 0 && <MessageSnackbar message={props.snackbarMessage} />}
     <Switch>
+      <Route exact path="/" component={HomePage} />
       <Route exact path="/about" component={AboutPage} />
-      <Route exact path="/join" component={JoinPage} />
-      <Route exact path="/login" component={LoginPage} />
+      {
+        props.isGuest &&
+        <Fragment>
+          <Route exact path="/join" component={JoinPage} />
+          <Route exact path="/login" component={LoginPage} />
+        </Fragment>
+      }
+      <Route path="/*" component={NotFoundPage} />
     </Switch>
   </div>
 );
 
+App.propTypes = {
+  isGuest: PropTypes.bool.isRequired,
+  snackbarMessage: PropTypes.string.isRequired,
+};
 
-export default App;
+const mapStateToProps = state => ({
+  isGuest: isEmpty(state.user),
+  snackbarMessage: state.snackbar,
+});
+
+export default connect(mapStateToProps)(App);
