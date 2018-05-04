6968f8ef1c3e722143122831ca46c25ea51f7e39
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-04 10:22:09 +0200

    Create NotFoundPage component

---
+++ src/components/pages/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+  <div>
+    <h1 className="weight300">You are lost!</h1>
+    <h2 className="weight300">You'll probably want to go back to Home page</h2>
+  </div>
+);
+
+export default NotFoundPage;
