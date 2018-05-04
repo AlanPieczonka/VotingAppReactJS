254ac7dd65e35096ca97fb093ffd50be46e8c3c5
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-04 16:16:27 +0200

    Add snackbar action types to types.js

--- src/types.js
+++ src/types.js
@@ -1,2 +1,4 @@
 export const USER_LOGGED_IN = 'USER_LOGGED_IN';
 export const USER_LOGGED_OUT = 'USER_LOGGED_OUT';
+export const SHOW_SNACKBAR = 'SHOW_SNACKBAR';
+export const CLOSE_SNACKBAR = 'CLOSE_SNACKBAR';
