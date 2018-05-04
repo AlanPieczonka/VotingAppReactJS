3e3435282f624df43d0601696509e187425ff394
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-04 09:57:26 +0200

    Create field/CreateTextField component

---
+++ src/components/fields/CreateTextField.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TextField from 'material-ui/TextField';
+
+const CreateTextField = ({
+  input,
+  label,
+  type,
+  fullWidth,
+  meta: { touched, error },
+}) => {
+  const isError = !!error;
+  return (
+    <TextField
+      label={label}
+      fullWidth={fullWidth}
+      type={type}
+      error={touched && isError}
+      helperText={error}
+      {...input}
+    />
+  );
+};
+
+CreateTextField.propTypes = {
+  input: PropTypes.object.isRequired,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  fullWidth: PropTypes.bool,
+  meta: PropTypes.shape({
+    touched: PropTypes.bool,
+    error: PropTypes.string,
+  }),
+};
+
+CreateTextField.defaultProps = {
+  fullWidth: true,
+  meta: {
+    touched: false,
+    error: null,
+  },
+};
+
+export default CreateTextField;
