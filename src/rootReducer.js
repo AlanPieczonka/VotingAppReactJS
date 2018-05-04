3c778be64d783c03bdf5f122b6a33bb0a735b853
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-04 10:36:08 +0200

    Add snackbar reducer to rootReducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import { reducer as form } from 'redux-form';
 import user from './reducers/user';
+import snackbar from './reducers/snackbar';
 
 const rootReducer = combineReducers({
   form,
   user,
+  snackbar,
 });
 
 export default rootReducer;
