864faffe58ca62cec7c04b9a3d00134ecef8e75c
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-15 21:24:58 +0200

    Fix typo in HomePage.js

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const NotFoundPage = () => (
+const HomePage = () => (
   <div>
     <h1 className="weight300">This is Home Page</h1>
     <h2 className="weight300">All polls</h2>
   </div>
 );
 
-export default NotFoundPage;
+export default HomePage;
