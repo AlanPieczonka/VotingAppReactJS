9ccd1d24e0b33bcad0851793202423699840e3e7
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-19 18:04:03 +0200

    Keep current user's id in Redux state and localStorage

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,6 @@
 import { USER_LOGGED_IN, USER_LOGGED_OUT } from '../types';
 import api from '../api';
+import setAuthorizationHeader from '../utils/setAuthorizationHeader';
 
 export const userLoggedIn = user => ({
   type: USER_LOGGED_IN,
@@ -9,6 +10,8 @@ export const userLoggedIn = user => ({
 export const login = credentials => dispatch =>
   api.user.login(credentials).then((user) => {
     localStorage.votingappJWT = user.token;
+    localStorage.votingappUserID = user.id;
+    setAuthorizationHeader(localStorage.votingappJWT);
     dispatch(userLoggedIn(user));
   });
 
@@ -18,5 +21,7 @@ export const userLoggedOut = () => ({
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem('votingappJWT');
+  localStorage.removeItem('votingappUserID');
+  setAuthorizationHeader();
   dispatch(userLoggedOut());
 };
