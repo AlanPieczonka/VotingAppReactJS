510ee8fe345bff2d27d17e52664352227a9e9b99
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-20 20:27:01 +0200

    Use PollsList component in HomePage component

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,51 +1,10 @@
-import React from 'react';
-import axios from 'axios';
-import { withStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import PollsList from '../reusable/PollsList';
 
-const styles = theme => ({
-  root: theme.mixins.gutters({
-    paddingTop: 16,
-    paddingBottom: 16,
-    marginTop: theme.spacing.unit * 3,
-  }),
-});
+const HomePage = () => (
+  <Fragment>
+    <PollsList endpoint="/polls" header="All polls" />
+  </Fragment>
+);
 
-class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      polls: [],
-    };
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:3000/polls').then(response => this.setState({ polls: response.data.polls }));
-  }
-
-  render() {
-    const { classes } = this.props;
-    const polls = this.state.polls.map(poll =>
-      (<div key={poll._id}>
-        <Link to={`polls/${poll._id}`} style={{ textDecoration: 'none' }}>
-          <Paper className={classes.root} elevation={4}>
-            <Typography variant="headline" component="h3">
-              {poll.title}
-            </Typography>
-          </Paper>
-        </Link>
-       </div>));
-    return (
-      <div className={classes.root}>
-        <List component="nav">
-          {polls}
-        </List>
-      </div>
-    );
-  }
-}
-
-export default withStyles(styles)(HomePage);
+export default HomePage;
