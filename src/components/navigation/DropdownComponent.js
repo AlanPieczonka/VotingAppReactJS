7696532667bdf8d3dc5c56aa6d6e3ff7c1165ab8
Alan Pieczonka <alanpieczonka@gmail.com>
2018-06-08 10:02:09 +0200

    Fix logout functionality

--- src/components/navigation/DropdownComponent.js
+++ src/components/navigation/DropdownComponent.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
@@ -6,78 +7,89 @@ import Menu from '@material-ui/core/Menu';
 import DropdownItem from './DropdownItem';
 import { showSnack } from '../../actions/snackbar';
 import { logout } from '../../actions/auth';
-import { connect } from 'react-redux';
+
+const LINKS = [
+  { name: 'About', to: '/about', type: 'public' },
+  { name: 'Join', to: '/join', type: 'guest' },
+  { name: 'Login', to: '/login', type: 'guest' },
+  { name: 'New Poll', to: '/polls/new', type: 'private' },
+  { name: 'Logout', to: '/', type: 'logout' },
+];
 
 class DropdownComponent extends Component {
 	state = {
-    anchorEl: null,
+	  anchorEl: null,
   };
-
+  
   handleClick = event => this.setState({ anchorEl: event.currentTarget });
   handleClose = () => this.setState({ anchorEl: null });
 
 	logoutAndShowSnack = () => {
-    this.props.logout();
-    showSnack('You have succesfully logged out!');
-  };
-	
-  render() {
-    const { userEmail, isAuthenticated } = this.props
-		const { anchorEl } = this.state
-		const links = [
-			{name: "New Poll", to: "/polls/new", type: "private"},
-			{name: "About", to: "/about", type: "public"},
-			{name: "Join", to: "/join", type: "guest"},
-			{name: "Login", to: "/login", type: "guest"},
-			{name: "Logout", to: "/", type: "private"}
-		]
-    return (
-      <div>
-				<IconButton
-          aria-label="More"
-          aria-owns={anchorEl ? 'long-menu' : null}
-          aria-haspopup="true"
-					onClick={this.handleClick}
-					color="inherit"
-        >
-          <MoreVertIcon />
-        </IconButton>
-				<Menu
-          id="long-menu"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={this.handleClose}
-          PaperProps={{
+	  this.props.logout();
+	  this.props.showSnack('You have succesfully logged out!');
+	};
+
+	render() {
+	  const { userEmail, isAuthenticated } = this.props;
+	  const { anchorEl } = this.state;
+	  const links = [
+	    { name: 'About', to: '/about', type: 'public' },
+	    { name: 'Join', to: '/join', type: 'guest' },
+	    { name: 'Login', to: '/login', type: 'guest' },
+	    { name: 'New Poll', to: '/polls/new', type: 'private' },
+	    { name: 'Logout', to: '/', type: 'logout' },
+	  ];
+	  return (
+  <div>
+    <IconButton
+      aria-label="More"
+      aria-owns={anchorEl ? 'long-menu' : null}
+      aria-haspopup="true"
+      onClick={this.handleClick}
+      color="inherit"
+    >
+      <MoreVertIcon />
+    </IconButton>
+    <Menu
+      id="long-menu"
+      anchorEl={anchorEl}
+      open={Boolean(anchorEl)}
+      onClose={this.handleClose}
+      PaperProps={{
             style: {
               width: 200,
             },
           }}
-        >
-					{userEmail && <DropdownItem link={{to: '/user/polls', name: userEmail}} handleClose={this.handleClose}/>}
-          {links.map(link => {
-						switch(link.type) {
-							case "guest":
+    >
+      {userEmail && <DropdownItem link={{ to: '/user/polls', name: userEmail }} handleClose={this.handleClose} />}
+      {LINKS.map((link) => {
+						switch (link.type) {
+              case 'public':
+                return (
+                  <DropdownItem key={link.name} link={link} handleClose={this.handleClose} />
+                );
+							case 'guest':
 								return !isAuthenticated && (
-									<DropdownItem key={link.name} link={link} handleClose={this.handleClose}/>
-								)
-							case "public":
-								return (
-									<DropdownItem key={link.name} link={link} handleClose={this.handleClose} onClick={this.logoutAndShowSnack}/>
-								)
-							case "private":
+									<DropdownItem key={link.name} link={link} handleClose={this.handleClose} />
+								);
+							case 'private':
 								return isAuthenticated && (
-									<DropdownItem key={link.name} link={link} handleClose={this.handleClose}/>
-								)
+									<DropdownItem key={link.name} link={link} handleClose={this.handleClose} />
+                );
+              case 'logout':
+                return isAuthenticated && (
+                  <DropdownItem key={link.name} link={link} handleClose={this.handleClose} onClick={this.logoutAndShowSnack} />
+                );
 							default:
 								return (
-									<DropdownItem key={link.name} link={link} handleClose={this.handleClose}/>
-								)
-						}							
-					})}
-        </Menu>
-      </div>
-    )
-  }
+  <DropdownItem key={link.name} link={link} handleClose={this.handleClose} />
+								);
+						}
+          })}
+    </Menu>
+  </div>
+	  );
+	}
 }
 
 DropdownComponent.propTypes = {
