df9258a27c1348258837d2cbcea353de175a22fb
Drakeoon <tmlm18@gmail.com>
2018-06-12 22:25:43 +0200

    Create RightPanel component

---
+++ src/components/pages/polls/RightPanel.js
@@ -0,0 +1,50 @@
+import React, { Fragment } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+export default ({ isAuthenticated, isAuthorized, newOption, handleChange }) => {
+  return (
+    <Grid item xs={4} md={3} xl={2}>
+      { isAuthenticated && (
+          <Paper elevation={4}>
+            <Fragment>
+              <div style={{ width: '100%', color: 'white', backgroundColor: '#2196F3', padding: '18px', boxSizing: 'border-box', textAlign: 'left'}}>
+                Missing options?
+              </div>
+              <div style={{ padding: '0 18px 36px 18px' }}>
+                <form onSubmit={this.addNewOption}>
+                  <TextField
+                    id="new-option"
+                    label="New option"
+                    margin="normal"
+                    value={newOption}
+                    onChange={handleChange('newOption')}
+                  />
+                  <br />
+                  <Button type="submit" variant="raised" size="small" color="primary">
+                    Add new option
+                  </Button>
+                </form>
+              </div>
+            </Fragment>
+          </Paper>
+      )}
+      { isAuthorized && (
+          <Paper elevation={4}>
+            <Fragment>
+              <div style={{ width: '100%', color: 'white', backgroundColor: '#F44336', padding: '18px', marginTop: '18px', boxSizing: 'border-box', textAlign: 'left'}}>
+                Danger Zone
+              </div>
+              <div style={{ padding: '18px' }}>
+                  <Button onClick={this.deletePoll} type="button" variant="raised" size="small" color="secondary">
+                      Delete Poll
+                  </Button>
+              </div>
+            </Fragment>
+          </Paper>
+      )}
+    </Grid>
+  )
+}
